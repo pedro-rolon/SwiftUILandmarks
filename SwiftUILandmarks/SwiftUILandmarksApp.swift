7fcc0a40966de83cac7c1eff036ad39e80666c2e
Pedro Rolon <pedro@MacBook-Pro-de-Pedro.local>
2020-12-29 20:21:02 -0300

    first commit

---
+++ SwiftUILandmarks/SwiftUILandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarksApp.swift
+//  SwiftUILandmarks
+//
+//  Created by Pedro Rolon on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
